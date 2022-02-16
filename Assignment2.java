d3714b224048d6612b1ff350a7e9da1c1e4cc410
tarunzemoso <tarun.pinnem@zemosolabs.com>
2022-02-16 19:08:46 +0530

    javaAssignments

--- Assignment2.java
+++ Assignment2.java
@@ -1,5 +1,4 @@
 import java.util.HashSet;
-import java.util.Locale;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -8,8 +7,8 @@ public class Assignment2 {
         Set<Character> set=new HashSet<>();
         input=input.toLowerCase();
         for(int index=0;index<input.length();index++){
-            if(input.charAt(index)>='a' && input.charAt(index)<='z') {
-                set.add(input.charAt(index));
+            if((input.charAt(index)>='a' && input.charAt(index)<='z')||(input.charAt(index)>='A' && input.charAt(index)<='Z')){
+                set.add(Character.toLowerCase(input.charAt(index)));
             }
         }
         if(set.size()!=26)
